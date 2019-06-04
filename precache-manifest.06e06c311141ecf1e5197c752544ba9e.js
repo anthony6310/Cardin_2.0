393c7cfbfada451b957d692adf9c5230b37f0138
anthony6310 <anthony6310322@yahoo.com>
2019-06-04 14:43:39 -0400

    Updates

---
+++ precache-manifest.06e06c311141ecf1e5197c752544ba9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de086dd81d87963ae0d38bfa6514b94a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72f4472624d232b12104",
+    "url": "/static/css/main.1e7f5b10.chunk.css"
+  },
+  {
+    "revision": "b989dac9dbb7bee4108c",
+    "url": "/static/js/2.ebecf277.chunk.js"
+  },
+  {
+    "revision": "72f4472624d232b12104",
+    "url": "/static/js/main.6903e9ef.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
